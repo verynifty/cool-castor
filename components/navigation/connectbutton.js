419928250768fe98bf0aaffb11642bd8c05a4811
grandsmarquis <jdourlens@gmail.com>
2024-02-07 16:24:48 +0900

    signin

--- components/navigation/connectbutton.js
+++ components/navigation/connectbutton.js
@@ -1,8 +1,67 @@
 'use client'
 
+import { useCallback, useEffect, useState } from "react";
+
 export default function Home() {
+
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
+    useEffect(() => {
+
+        window.onSignInSuccess = async (data) => {
+            localStorage.setItem("fid", data.fid);
+            localStorage.setItem("signer", data.signer_uuid);
+            const res = await fetch(
+                `/api/user/get?fid=${data.fid}`
+            );
+            const user = await res.json();
+            localStorage.setItem("user", data.signer_uuid);
+            localStorage.setItem("connected", true);
+
+            setIsSignedIn(true);
+        };
+
+        // Identify or create the script element
+        let script = document.getElementById(
+            "siwn-script"
+        );
+
+        if (!script) {
+            script = document.createElement("script");
+            script.id = "siwn-script";
+            document.body.appendChild(script);
+        }
+
+        // Set attributes and source of the script
+        script.src = "https://neynarxyz.github.io/siwn/raw/1.2.0/index.js";
+        script.async = true;
+        script.defer = true;
+
+        document.body.appendChild(script);
+
+        return () => {
+            // Remove the script from the body
+            if (script) {
+                document.body.removeChild(script);
+            }
+
+            // Remove the button if it exists
+            let button = document.getElementById("siwn-button");
+            if (button && button.parentElement) {
+                button.parentElement.removeChild(button);
+            }
+        };
+    }, [
+    ]);
+
+
     return (
         <div>
+            <div
+                className="neynar_signin mt-6"
+                data-client_id="1989362d-385f-47da-9ad4-945b49afb49c"
+                data-success-callback="onSignInSuccess"
+            ></div>
         </div>
     );
 }
