43fdf1e225b7844f482e6079b00a3ab4547161b7
grandsmarquis <jdourlens@gmail.com>
2024-02-10 19:01:14 +0900

    table

--- components/table/usertable.js
+++ components/table/usertable.js
@@ -5,6 +5,9 @@ import { useEffect, useState } from "react";
 export default function Home(props) {
 
     const [isSignedIn, setIsSignedIn] = useState(false);
+    const [maxFollowers, setMaxFollowers] = useState(0);
+    const [maxFollowing, setMaxFollowing] = useState(0);
+
     const columns = [
         {
             name: 'Name',
@@ -23,25 +26,50 @@ export default function Home(props) {
             </div>
         },
         {
+            name: 'Since',
+            selector: row => row.timestamp,
+            sortable: true,
+         
+        },
+        {
             name: 'Follower Count',
             selector: row => row.followerCount,
             sortable: true,
+            cell: row => <div>
+                <div>{row.followerCount}</div> <progress className="progress w-56" value={row.followerCount} max={maxFollowers}></progress>
+            </div>
+
 
         },
         {
             name: 'Following Count',
             selector: row => row.followingCount,
             sortable: true,
+            cell: row => <div>
+                <div>{row.followingCount}</div> <progress className="progress w-56" value={row.followingCount} max={maxFollowing}></progress>
+            </div>
 
         },
     ];
 
     useEffect(() => {
-
+        let following = 0;
+        let followers = 0;
+        for (let user of props.users) {
+            if (user.followerCount > followers) {
+                followers = user.followerCount;
+            }
+            if (user.followingCount > following) {
+                following = user.followingCount
+            }
+        }
+        setMaxFollowers(followers);
+        setMaxFollowing(following);
 
 
 
     }, [
+        props.users
     ]);
 
     if (isSignedIn) {
